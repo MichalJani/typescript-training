68c674bd6d5fd3ac5436a460b23846db70e6b391
Michal Janicki <michal.janicki1@gmail.com>
2019-10-13 13:11:55 +0200

    Add more examples

---
+++ classes.ts
@@ -0,0 +1,7 @@
+class User {
+  name: string;
+  email: string;
+  age: number;
+
+  constructor() {}
+}
---
+++ types.ts
@@ -0,0 +1,31 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+// let strArr: string[];
+// let numArr: number[];
+// let boolArr: boolean[];
+
+let strArr: Array<string>;
+let numArr: Array<number>;
+let boolArr: Array<boolean>;
+
+let strNumTuple: [string, number];
+
+myString = 'Hello'.slice(0, 3);
+myNum = +5.6 - 22;
+myBool = true;
+myVar = true;
+
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 3];
+boolArr = [true, false];
+
+strNumTuple = ['Hello', 3];
+
+let myVoid: void = null;
+let myNull: null = null;
+let myUndefined: undefined = undefined;
+
+console.log(myVoid);
